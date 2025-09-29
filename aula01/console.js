6199ca064fff12af4727e859c480622d24e8c42e
Nocturnnn <nocturn2352@gmail.com>
2025-09-29 16:46:30 -0300

    comeco

---
+++ aula01/console.js
@@ -0,0 +1,3 @@
+console.log("Olá, 'mundo'"); //String
+console.log(1234567, 666888.99); //Number
+console.log("Mateus tem", 99.99, "piranhas");
---
+++ aula01/ex/index.js
@@ -0,0 +1,6 @@
+//exibir: Meu nome é "NOME". Estou aprendedno JavaScript às 10 da manhã.
+console.log(
+  'Meu nome é "Mateus". Estou aprendendo JavaScript às',
+  10,
+  "da manhã."
+);
