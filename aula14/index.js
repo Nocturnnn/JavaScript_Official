f6fa254bcaacedda5e5369f207cfed5cfb932131
Nocturnnn <nocturn2352@gmail.com>
2025-10-08 13:55:25 -0300

    numbers

---
+++ aula14/index.js
@@ -0,0 +1,13 @@
+let num1 = 10.4
+let num2 = 5
+
+console.log(num1.toString() + num2) //transformei um número em uma string
+console.log(num1.toString(2)) //o número em binário
+console.log(num1.toFixed(2)) //formatar o número
+console.log(Number.isInteger(num2)) //retona true se o número for inteiro e vise versa
+console.log(Number.isNaN(num2)) //retona true se o número for NaN e vise versa
+
+let n1 = 0.4
+let n2 = 0.6
+const resultado = n1 + n2
+console.log(resultado)
