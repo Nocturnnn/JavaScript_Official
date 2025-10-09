05d1c8d0acef557cd6ea16c93fcf9cf516c5fcfe
Nocturnnn <nocturn2352@gmail.com>
2025-10-09 14:52:29 -0300

    array

---
+++ aula17/array.js
@@ -0,0 +1,9 @@
+let alunos = ['Mateus', 'Maria', 'Napoleão']
+console.log(alunos[0])
+console.log(alunos)
+alunos.push('José') // adicionar elementos ao final do array
+alunos.unshift('Gabriel') // adicionar elementos ao começo do array
+console.log(alunos)
+alunos.pop() //remover elementos do final do array
+alunos.shift() //remover elementos do começo do array
+console.log(alunos)
