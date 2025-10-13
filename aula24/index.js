62b188a431f3749a3a804d6171e4f6ac32ad2e18
Nocturnnn <nocturn2352@gmail.com>
2025-10-13 15:58:16 -0300

    If e modelo

---
+++ aula24/index.js
@@ -0,0 +1,8 @@
+function FalaOi(){
+    return 'Oi'
+}
+
+const vaiExecutar = false
+
+console.log(vaiExecutar && FalaOi()) // and
+console.log(0 || false || null || 'Mateus' || true)
---
+++ aula25/index.js
@@ -0,0 +1,9 @@
+const hora = 13
+
+if(hora <= 12 && hora >= 0){
+    console.log('Bom dia!!')
+}else if(hora > 12 && hora <= 17){
+    console.log('Boa tarde!!')
+}else{
+    console.log('Boa noite!!')
+}
